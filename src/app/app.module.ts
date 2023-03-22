a877eb87730fc7d6cf3a3bf547eea53e48d2a69d
marcokr02 <marcokr02@gmail.com>
2023-03-22 17:36:45 +0100

    Nochmal Änderungen aus dem Call

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,8 +8,10 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { RouterModule, Routes } from '@angular/router';
 import { AccountComponent } from './account/account.component';
-import { AnmeldenComponent } from './anmelden/anmelden.component';
 import { HeaderComponent } from './header/header.component';
+import { HomepageComponent } from './homepage/homepage.component';
+import { RegistrierungComponent } from './registrierung/registrierung.component';
+import { AnmeldungComponent } from './anmeldung/anmeldung.component';
 
 const meineRouten:Routes = [
   {path: '', component: HeaderComponent},
@@ -23,7 +25,9 @@ const meineRouten:Routes = [
     HeaderComponent,
     FooterComponent,
     AccountComponent,
-    AnmeldenComponent
+    HomepageComponent,
+    RegistrierungComponent,
+    AnmeldungComponent
   ],
   imports: [
     BrowserModule,
--- src/app/registrierung/registrierung.component.spec.ts
+++ src/app/registrierung/registrierung.component.spec.ts
@@ -1,18 +1,18 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { AnmeldenComponent } from './anmelden.component';
+import { RegistrierungComponent } from './registrierung.component';
 
-describe('AnmeldenComponent', () => {
-  let component: AnmeldenComponent;
-  let fixture: ComponentFixture<AnmeldenComponent>;
+describe('RegistrierungComponent', () => {
+  let component: RegistrierungComponent;
+  let fixture: ComponentFixture<RegistrierungComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ AnmeldenComponent ]
+      declarations: [ RegistrierungComponent ]
     })
     .compileComponents();
 
-    fixture = TestBed.createComponent(AnmeldenComponent);
+    fixture = TestBed.createComponent(RegistrierungComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
