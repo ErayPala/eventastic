dfbde631c18155944e7b4cb315b9b008b147e2aa
Eray Pala <peray55@gmail.com>
2023-03-22 16:29:09 +0100

    Neue Komponenten

---
+++ src/app/anmelden/anmelden.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-anmelden',
+  templateUrl: './anmelden.component.html',
+  styleUrls: ['./anmelden.component.scss']
+})
+export class AnmeldenComponent {
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,17 +8,30 @@ import { HeaderKBComponent } from './header-kb/header-kb.component';
 import { FooterComponent } from './footer/footer.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+import { RouterModule, Routes } from '@angular/router';
+import { AccountComponent } from './account/account.component';
+import { AnmeldenComponent } from './anmelden/anmelden.component';
+
+const meineRouten:Routes = [
+{path: 'footer', component: FooterComponent},
+{path: 'header-nkb', component: HeaderNKBComponent},
+{path: '', component: HeaderNKBComponent}
+];
+
 @NgModule({
   declarations: [
     AppComponent,
     HeaderNKBComponent,
     HeaderKBComponent,
-    FooterComponent
+    FooterComponent,
+    AccountComponent,
+    AnmeldenComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    RouterModule.forRoot(meineRouten)
   ],
   providers: [],
   bootstrap: [AppComponent]
