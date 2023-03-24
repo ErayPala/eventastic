ef18c51b5e94e2cc98316c4bc3d59e80666804e8
marcokr02 <marcokr02@gmail.com>
2023-03-24 15:09:12 +0100

    app.module.ts sortiert (also Ordnung geschaffen)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,27 +3,36 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { FooterComponent } from './footer/footer.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
 import { RouterModule, Routes } from '@angular/router';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule } from "@angular/common/http";
+
+//Hier sind alle von uns erstellten Komponenten durch ng g c ...
 import { AccountComponent } from './account/account.component';
-import { HeaderComponent } from './header/header.component';
 import { HomepageComponent } from './homepage/homepage.component';
 import { RegistrierungComponent } from './registrierung/registrierung.component';
 import { AnmeldungComponent } from './anmeldung/anmeldung.component';
-import { HttpClientModule } from "@angular/common/http";
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 import { ErstellenComponent } from './erstellen/erstellen.component';
-import {MatFormFieldModule} from '@angular/material/form-field';
-import {MatSelectModule} from '@angular/material/select';
+
+//Hier sind alle Module von Angular Material Desgin importiert
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatSelectModule } from '@angular/material/select';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatRadioModule } from '@angular/material/radio';    
 
 const meineRouten:Routes = [
   {path: '', component: HomepageComponent},
+  {path: 'homepage', component: HomepageComponent},
+  {path: 'registrierung', component: RegistrierungComponent},
+  {path: 'anmeldung', component: AnmeldungComponent},
+  {path: 'erstellen', component: ErstellenComponent},
   {path: 'header', component: HeaderComponent},
   {path: 'footer', component: FooterComponent},
-  {path: 'erstellen', component: ErstellenComponent},
-  {path: 'homepage', component: HomepageComponent},
-  {path: 'anmeldung', component: AnmeldungComponent},
 ];
 
 @NgModule({
@@ -31,20 +40,27 @@ const meineRouten:Routes = [
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    AccountComponent,
     HomepageComponent,
     RegistrierungComponent,
     AnmeldungComponent,
+    AccountComponent,
     ErstellenComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
+    RouterModule.forRoot(meineRouten),
     BrowserAnimationsModule,
+    HttpClientModule,
+    //Ab hier sind wieder die zugehörigen imports für Angular Material Design
     MatFormFieldModule,
     MatSelectModule,
-    HttpClientModule,
-    RouterModule.forRoot(meineRouten)
+    MatButtonModule,
+    MatDividerModule,
+    MatIconModule,
+    MatInputModule,
+    MatRadioModule,
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
