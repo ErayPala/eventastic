3bfb09acdd4d080651d79d47bf6f985f8df7a484
marcokr02 <marcokr02@gmail.com>
2023-04-21 02:34:04 +0200

    A lot of changes

--- server/server.js
+++ server/server.js
@@ -4,6 +4,8 @@ const express = require('express');
 
 // Database
 const mysql = require('mysql');
+
+const cors = require('cors');
 // Database connection info - used from environment variables
 var dbInfo = {
     connectionLimit : 10,
@@ -41,120 +43,59 @@ const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// Entrypoint - call it with: http://localhost:8080/ -> redirect you to http://localhost:8080/static
-app.get('/', (req, res) => {
-    console.log("Got a request and redirect it to the static page");
-    // redirect will send the client to another path / route. In this case to the static route.
-    res.redirect('/static');
-});
+const corsOptions = {
+    origin: 'http://localhost:4200',
+    credentials: true,
+    methods: 'GET, POST, PUT, DELETE',
+    allowedHeaders: 'Content-Type, Authorization',
+    exposedHeaders: 'Authorization'
+};
 
-// Another GET Path - call it with: http://localhost:8080/special_path
-app.get('/special_path', (req, res) => {
-    res.send('This is another path');
-});
+app.use(cors(corsOptions));
 
-// Another GET Path that shows the actual Request (req) Headers - call it with: http://localhost:8080/request_info
-app.get('/request_info', (req, res) => {
-    console.log("Request content:", req)
-    res.send('This is all I got from the request:' + JSON.stringify(req.headers));
-});
+app.get('/api/test', (req, res) => {
 
-// POST Path - call it with: POST http://localhost:8080/client_post
-app.post('/client_post', (req, res) => {
-    if (typeof req.body !== "undefined" && typeof req.body.post_content !== "undefined") {
-        var post_content = req.body.post_content;
-        console.log("Client send 'post_content' with content:", post_content)
-        // Set HTTP Status -> 200 is okay -> and send message
-        res.status(200).json({ message: 'I got your message: ' + post_content });
-    }
-    else {
-        // There is no body and post_contend
-        console.error("Client send no 'post_content'")
-        // Set HTTP Status -> 400 is client error -> and send message
-        res.status(400).json({ message: 'This function requries a body with "post_content"' });
-    }
-});
+    connection.query("SELECT * FROM `events`", function (error, results, fields) {
 
-// ###################### BUTTON EXAMPLE ######################
-// POST path for Button 1
-app.post('/button1_name', (req, res) => {
-    // Load the name from the formular. This is the ID of the input:
-    const name = req.body.name
-    // Print it out in console:
-    console.log("Client send the following name: " + name + " | Button1")
-    // Send JSON message back - this could be also HTML instead.
-    res.status(200).json({ message: 'I got your message - Name is: ' + name });
-    // More information here: https://developer.mozilla.org/en-US/docs/Learn/Server-side/Express_Nodejs/forms
-})
-
-// GET path for Button 2
-app.get('/button2', (req, res) => {
-    // This will generate a random number and send it back:
-    const random_number = Math.random();
-    // Print it out in console:
-    console.log("Send the following random number to the client: " + random_number + " | Button2")
-    // Send it to the client / webbrowser:
-    res.send("Antwort: g" + random_number);
-    // Instead of plain TXT - the answer could be a JSON
-    // More information here: https://www.w3schools.com/xml/ajax_intro.asp
-});
-// ###################### BUTTON EXAMPLE END ######################
-
-
-// ###################### DATABASE PART ######################
-// GET path for database
-app.get('/database', (req, res) => {
-    console.log("Request to load all entries from table1");
-    // Prepare the get query
-    connection.query("SELECT * FROM `table1`;", function (error, results, fields) {
         if (error) {
-            // we got an errror - inform the client
-            console.error(error); // <- log error in server
-            res.status(500).json(error); // <- send to client
+            console.error(error);
+            res.status(500).json(error);
         } else {
-            // we got no error - send it to the client
-            console.log('Success answer from DB: ', results); // <- log results in console
-            // INFO: Here could be some code to modify the result
-            res.status(200).json(results); // <- send it to client
+            console.log('Success answer from DB: ', results);
+            res.status(200).json(results);
         }
     });
 });
 
-// DELETE path for database
-app.delete('/database/:id', (req, res) => {
-    // This path will delete an entry. For example the path would look like DELETE '/database/5' -> This will delete number 5
-    let id = req.params.id; // <- load the ID from the path
-    console.log("Request to delete Item: " + id); // <- log for debugging
+app.post('/api/erstellen', (req, res) => {
 
-    // Actual executing the query to delete it from the server
-    // Please keep in mind to secure this for SQL injection!
-    connection.query("DELETE FROM `table1` WHERE `table1`.`task_id` = " + id + ";", function (error, results, fields) {
-        if (error) {
-            // we got an errror - inform the client
-            console.error(error); // <- log error in server
-            res.status(500).json(error); // <- send to client
-        } else {
-            // Everything is fine with the query
-            console.log('Success answer: ', results); // <- log results in console
-            // INFO: Here can be some checks of modification of the result
-            res.status(200).json(results); // <- send it to client
-        }
-    });
-});
+    console.log('Erfolgreiche Server-Coonect oin'); // <- log results in console
 
-// POST path for database
-app.post('/database', (req, res) => {
     // This will add a new row. So we're getting a JSON from the webbrowser which needs to be checked for correctness and later
     // it will be added to the database with a query.
-    if (typeof req.body !== "undefined" && typeof req.body.title !== "undefined" && typeof req.body.description !== "undefined") {
+    if (typeof req.body !== "undefined" && typeof req.body.eventtitel !== "undefined" && typeof req.body.veranstalter !== "undefined" && typeof req.body.typ !== "undefined" 
+    && typeof req.body.kategorie !== "undefined" && typeof req.body.adresse !== "undefined" && typeof req.body.stadt !== "undefined" && typeof req.body.bundesland !== "undefined"
+    && typeof req.body.plz !== "undefined" && typeof req.body.land !== "undefined" && typeof req.body.startdatum !== "undefined" && typeof req.body.enddatum !== "undefined" 
+    && typeof req.body.startzeit !== "undefined" && typeof req.body.endzeit !== "undefined") {
         // The content looks good, so move on
         // Get the content to local variables:
-        var title = req.body.title;
-        var description = req.body.description;
-        console.log("Client send database insert request with 'title': " + title + " ; description: " + description); // <- log to server
+        var eventtitel = req.body.eventtitel;
+        var veranstalter = req.body.veranstalter;
+        var typ = req.body.typ;
+        var kategorie = req.body.kategorie;
+        var adresse = req.body.adresse;
+        var stadt = req.body.stadt;
+        var bundesland = req.body.bundesland;
+        var plz = req.body.plz;
+        var land = req.body.land;
+        var startdatum = req.body.eventbeginn;
+        var enddatum = req.body.eventende;
+        var startzeit = req.body.startzeit;
+        var endzeit = req.body.uhrzeit;
+        console.log("Client send database insert request with 'eventtitel': " + eventtitel + " ; veranstalter: " + veranstalter); // <- log to server
         // Actual executing the query. Please keep in mind that this is for learning and education.
         // In real production environment, this has to be secure for SQL injection!
-        connection.query("INSERT INTO `table1` (`task_id`, `title`, `description`, `created_at`) VALUES (NULL, '" + title + "', '" + description + "', current_timestamp());", function (error, results, fields) {
+        connection.query("INSERT INTO `events` (`event_id`, `event_titel`, `event_veranstalter`, `event_typ`, `event_kategorie`, `event_adresse`, `event_stadt`,`event_bundesland`, `event_plz`, `event_land`, `event_startdatum`, `event_enddatum`, `event_startzeit`, `event_endzeit`, `event_erstellt`) VALUES (NULL, '" + eventtitel + "', '" + veranstalter + "', '" + typ + "', '" + kategorie + "', '" + adresse + "', '" + stadt + "', '" + bundesland + "', '" + plz + "', '" + land + "', '" + startdatum + "', '" + enddatum + "', '" + startzeit + "', '" + endzeit + "' current_timestamp());", function (error, results, fields) {
             if (error) {
                 // we got an errror - inform the client
                 console.error(error); // <- log error in server
@@ -174,11 +115,6 @@ app.post('/database', (req, res) => {
         res.status(400).json({ message: 'This function requries a body with "title" and "description' });
     }
 });
-// ###################### DATABASE PART END ######################
-
-// All requests to /static/... will be redirected to static files in the folder "public"
-// call it with: http://localhost:8080/static
-app.use('/static', express.static('public'))
 
 // Start the actual server
 app.listen(PORT, HOST);
--- src/app/erstellen/erstellen.component.ts
+++ src/app/erstellen/erstellen.component.ts
@@ -1,11 +1,68 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Router } from '@angular/router';
+import { FormGroup, FormControl } from '@angular/forms';
+import { OnInit } from '@angular/core';
+import { Validators } from '@angular/forms';
+
+const baseUrl = "http://localhost:8080/"
 
 @Component({
-  selector: 'app-erstellen',
-  templateUrl: './erstellen.component.html',
-  styleUrls: ['./erstellen.component.scss'],
+    selector: 'app-erstellen',
+    templateUrl: './erstellen.component.html',
+    styleUrls: ['./erstellen.component.scss'],
 })
-export class ErstellenComponent {
 
+export class ErstellenComponent implements OnInit {
+
+    ErstellungForm: FormGroup;
+
+    constructor(private http: HttpClient, private router: Router) { }
+
+    serverAntwort: any
+
+    ngOnInit() {
+        this.http.get<any[]>('http://localhost:8080/api/test').subscribe(data => {
+            console.log(data)
+        })
+        this.ErstellungForm = new FormGroup(
+            {
+                eventtitel: new FormControl('', [Validators.required]),
+                veranstalter: new FormControl('', [Validators.required]),
+                typ: new FormControl('', [Validators.required]),
+                kategorie: new FormControl('', [Validators.required]),
+                adresse: new FormControl('', [Validators.required]),
+                stadt: new FormControl('', [Validators.required]),
+                bundesland: new FormControl('', [Validators.required]),
+                plz: new FormControl('', [Validators.required]),
+                land: new FormControl('', [Validators.required]),
+                eventbeginn: new FormControl('', [Validators.required]),
+                startzeit: new FormControl('', [Validators.required]),
+                eventende: new FormControl('', [Validators.required]),
+                uhrzeit: new FormControl('', [Validators.required]),
+            }
+        )
+    }
+
+    submit() {
+
+        console.log('Submit Funzt')
+
+        const headers = { 'content-type': 'application/json' }
+        const body = JSON.stringify(this.ErstellungForm.getRawValue);
+        // const body = JSON.stringify({message: "Digga"});
+        console.log('Immerhin des ');
+        console.log(this.ErstellungForm);
+        this.http.post('http://localhost:8080/api/erstellen', body, { headers: headers }).subscribe((response) => {
+            console.log(response);
+            this.serverAntwort = response;
+            // this.router.navigate(['/erstellen'], {state: {serverAntwort: this.serverAntwort}});
+        },
+            (error) => {
+                console.log(error);
+                console.log('Ist des n Error oder was')
+                this.serverAntwort = error.error.message;
+            },
+        );
+    }
 }
-
